7f20c3651c1b5b6b645c73f8c029651f418e3d19
yongLivelo <julioeduardolivelo@gmail.com>
2026-04-30 21:44:23 +0800

    perfect

--- .config/fish/functions/ll.fish
+++
@@ -1,3 +0,0 @@
-function ll --wraps='eza --icons --git -lh --group-directories-first' --description 'alias ll=eza --icons --git -lh --group-directories-first'
-    eza --icons --git -lh --group-directories-first $argv
-end
--- .config/fish/functions/ls.fish
+++
@@ -1,3 +0,0 @@
-function ls --wraps='eza --icons --git --group-directories-first' --description 'alias ls=eza --icons --git --group-directories-first'
-    eza --icons --git --group-directories-first $argv
-end
