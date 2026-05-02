716c5575f97928067082b5810389a988efe0ef44
Livelo Julio B. <julioeduardolivelo@gmail.com>
2026-05-02 22:56:41 +0800

    hybrid keybinds for fish and mouse on for tmux panes

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,6 @@
 if status is-interactive
     oh-my-posh init fish --config ~/.config/fish/theme.json | source
-    set -g fish_key_bindings fish_vi_key_bindings
+    set -g fish_key_bindings fish_hybrid_key_bindings
     fzf_key_bindings
     bind -M insert -m default jk backward-char force-repaint
     fish_config theme choose tomorrow-night-bright
