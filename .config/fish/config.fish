8840649504be0fc057e74a0684bff255bbb431fa
Livelo Julio B. <julioeduardolivelo@gmail.com>
2026-05-02 14:22:14 +0800

    added automatic ssh login for fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,8 +1,9 @@
 if status is-interactive
     oh-my-posh init fish --config ~/.config/fish/theme.json | source
-    set -g fish_greeting
     set -g fish_key_bindings fish_vi_key_bindings
     fzf_key_bindings
     bind -M insert -m default jk backward-char force-repaint
     fish_config theme choose tomorrow-night-bright
+    set -g fish_greeting
+    fish_ssh_agent
 end
