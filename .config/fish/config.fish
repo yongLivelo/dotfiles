7e863814ef825a44a234445cc900fb86861a83e0
Livelo Julio B. <julioeduardolivelo@gmail.com>
2026-05-07 14:05:06 +0800

    added eza, zoxide integration to shell and added fzf themes

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -23,4 +23,13 @@ if status is-interactive
     end
 
     alias n='nvim'
+
+    zoxide init --cmd cd fish | source
+    set -Ux FZF_DEFAULT_OPTS "
+     --color=fg:#c0caf5,bg:#1a1b26,hl:#bb9af7 \
+    --color=fg+:#c0caf5,bg+:#24283b,hl+:#7dcfff \
+    --color=info:#7aa2f7,prompt:#7dcfff,pointer:#7dcfff \
+    --color=marker:#9ece6a,spinner:#9ece6a,header:#9ece6a
+    "
+    alias ls eza
 end
