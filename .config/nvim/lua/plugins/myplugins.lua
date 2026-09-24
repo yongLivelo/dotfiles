d27f9ba91d4cd0bda644c4d95179f5178ede0909
julio <julioeduardolivelo@gmail.com>
2026-09-25 07:41:16 +0800

    aslkdjaskldj

--- .config/nvim/lua/plugins/myplugins.lua
+++ .config/nvim/lua/plugins/myplugins.lua
@@ -163,14 +163,16 @@ return {
   },
   {
     "kawre/leetcode.nvim",
-    build = ":TSUpdate html", -- if you have `nvim-treesitter` installed
+    build = ":TSUpdate html",
     dependencies = {
-      -- include a picker of your choice, see picker section for more details
       "nvim-lua/plenary.nvim",
       "MunifTanjim/nui.nvim",
     },
     opts = {
       lang = "python3",
+      plugins = {
+        non_standalone = true,
+      },
     },
   },
   {
